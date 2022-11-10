2f78948aa5d7b643b55e0db876827ab6747984bf
badboy <115893602+badboy-ae@users.noreply.github.com>
2022-11-10 16:49:53 +0330

    Add files via upload

---
+++ Comparison.py
@@ -0,0 +1,14 @@
+# #!user/bin/python
+# coding=utf-8
+
+x = input("enter first choice : ")
+y = input("enter secend choice : ")
+
+
+print("x > y :",x > y)
+print("x < y :",x < y)
+print("x >= y :",x >= y)
+print("x <= y :",x <= y)
+print("x == y :",x == y)
+print("x != y :",x != y)
+
---
+++ calculator.py
@@ -0,0 +1,13 @@
+# #!user/bin/python
+# coding=utf-8
+
+x = float(input("enter your first number :"))
+y = float(input("enter your secend number :"))
+
+print("sum :",x+y)
+print("negetive :" , x-y )
+print("zarb :" , x * y )
+print("devide :", x / y)
+print("graond devide :", x//y )
+print("tavan :", x ** y)
+print("remain :", x % y)
